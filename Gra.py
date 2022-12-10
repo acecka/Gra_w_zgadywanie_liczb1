afa6cc3edcc76862ff678deed0d86a80653338e9
acecka <agn.cecka@gmail.com>
2022-12-10 15:42:59 +0100

    Zadanie zrobione

---
+++ Gra.py
@@ -0,0 +1,22 @@
+from random import randint
+
+number = randint(1, 101)
+
+while True:
+
+    try:
+
+        choice = int(input("Guess the number:"))
+
+        if choice == number:
+            print("You win!")
+            break
+
+        if choice < number:
+            print("Too small!")
+
+        if choice > number:
+            print("Too big!")
+
+    except ValueError:
+        print("It's not a number")
